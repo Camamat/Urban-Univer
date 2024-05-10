5e98877bd4623a4c3c73c7d0a7b545f44d803108
Camamat <117739993+Camamat@users.noreply.github.com>
2024-05-10 11:02:50 +0300

    homework5.py

--- homework5.py
+++ homework5.py
@@ -2,7 +2,7 @@ my_list = ['apple', 'banana', 'orange', 'avocado', 'pear']
 print(my_list)
 print(my_list[0])
 print(my_list[-1])
-print(my_list[3:6])
+print(my_list[2:5])
 my_list[3] = 123
 print(my_list)
 
