b4190718fd0857705bc537327ecc86a38577a1e8
Camamat <117739993+Camamat@users.noreply.github.com>
2024-05-10 10:39:54 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'banana', 'orange', 'avocado', 'pear']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:6])
+my_list[3] = 123
+print(my_list)
+
+
+my_dict = {'one' : 1, 'two' : 2, 'three' : 3, 'four' : 4}
+print(my_dict)
+print(my_dict.get('one'))
+my_dict['one'] = 5
+print(my_dict)
