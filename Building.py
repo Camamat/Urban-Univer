dd62bfca1d4596cf0af9006db27b180e1703f444
Camamat <117739993+Camamat@users.noreply.github.com>
2024-06-12 11:12:12 +0300

    Add files via upload

---
+++ Building.py
@@ -0,0 +1,10 @@
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+
+    def __eq__(self, other):
+        return self.numberOfFloors == self.buildingType
+
+Floor = Building(10, 5)
+print(Floor == Floor)
