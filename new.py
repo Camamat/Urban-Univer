e79a7a5dea7210d7e04df7ca24f207d60c93a3af
Camamat <117739993+Camamat@users.noreply.github.com>
2024-06-30 10:41:27 +0300

    Add files via upload

--- new.py
+++ new.py
@@ -1,6 +1,30 @@
-def test_function():
-    def inner_function():
-        print('Я в области видимости функции test_function')
-    inner_function()
-test_function()
-inner_function()
+team1_num = 6
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    challenge_result = 'Победа команды Мастера кода!'
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    challenge_result = 'Победа команды Волшебники Данных!'
+else:
+    challenge_result = 'Ничья!'
+
+
+print('В команде Мастера кода участников: %s !' % ('5'))
+
+print('Итого сегодня в командах участников: %s и %s !' % ('5', '6'))
+
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+
+print('Волшебники данных решили задачи за {} с !'.format(team2_time))
+
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+print(f'Результат битвы: {challenge_result}')
+
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
