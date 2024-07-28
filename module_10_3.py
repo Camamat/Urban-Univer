d5ee94826e30b99af6a190a5f3b51930d5301c82
Camamat <117739993+Camamat@users.noreply.github.com>
2024-07-28 10:21:31 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,42 @@
+import threading
+from threading import Thread, Lock
+
+lock = Lock()
+
+class BankAccount(Thread):
+
+    def __init__(self):
+        super().__init__()
+        self.balance = 1000
+
+    def deposit(self, amount):
+        with lock:
+            self.amount = amount
+            self.balance = self.balance + amount
+            print(f'Deposited {amount}, new balance is {self.balance}')
+
+    def withdraw(self, amount):
+        with lock:
+            self.amount = amount
+            self.balance = self.balance - amount
+            print(f'Withdrew {amount}, new balance is {self.balance}')
+
+
+def deposit_task(account, amount):
+        for _ in range(5):
+            account.deposit(amount)
+
+def withdraw_task(account, amount):
+        for _ in range(5):
+            account.withdraw(amount)
+
+account = BankAccount()
+
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
