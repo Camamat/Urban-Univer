31b57e2e445aa839f138c03cd66f9f53873e670b
Camamat <117739993+Camamat@users.noreply.github.com>
2024-08-20 18:37:49 +0300

    Add files via upload

--- introspek_2.py
+++ introspek_2.py
@@ -1,14 +1,13 @@
 def introspection_info(obj):
     dictionary = {}
     dictionary['type'] = type(obj)
-    dictionary['attributes'] =  [atribut for atribut in dir(obj) if '__' in atribut]
-    dictionary['methods'] = [method for method in dir(obj) if '__' not in method]
+    for x in dir(obj):
+        if not callable(getattr(obj, f'{x}')):
+            dictionary['attributes'] = getattr(obj, f'{x}')
+    dictionary['methods'] = [method for method in dir(obj) if '' in method]
     dictionary['module'] = introspection_info.__module__
     return dictionary
 
-
-
 number_info = introspection_info(42)
 print(number_info)
-
-# {'type': 'int', 'attributes': ['__abs__', '__add__', ...], 'methods': [], 'module': '__main__'}
+# {'type': 'int', 'attributes': [...], 'methods': ['__abs__', '__add__', ...], 'module': '__main__'}
