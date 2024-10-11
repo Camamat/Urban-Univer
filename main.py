8955bf3d958f133f794764801bbeae97aca7e582
Camamat <117739993+Camamat@users.noreply.github.com>
2024-10-11 16:31:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-x = 38
-print('дратути!')
-if x < 0:
-    print('меньше нуля')
-print('дотвидания')
+from fastapi import FastAPI
+from app.routers import user, task
 
-a, b = 10, 5
-if a > b:
-    print('a > b')
-if a > b and a > 0:
-    print('успех')
-if (a>b) and (a > 0 or b < 1000):
-    print('успех')
-if 5 < b and b < 10:
-    print('успех')
+app = FastAPI()
 
-if '34' > '123':
-    print('успех')
-if '123' > '12':
-    print('успех')
-if [1, 2] > [1, 1]:
-    print('успех')
+@app.get('/')
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
 
-if '6' > 5:
-    print('успех')
-if [5, 6] > 5:
-    print('успех')
-if '6' != 5:
-    print('успех')
+app.include_router(user.router)
+app.include_router(task.router)
